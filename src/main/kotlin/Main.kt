a17fdd9e9984e01c1a0cf0ac609149a4823277f7
rh0danthe <rita.pichushkina.04@mail.ru>
2024-10-28 17:56:06 +0300

    fix

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -6,19 +6,27 @@ import utils.Validator
 fun main(args: Array<String>) {
     try{
         if (args.isEmpty()) {
-            val io = ConsoleIO()
-            val service = CalculateService(io)
-            service.execute()
+            calculateFromConsole()
             return
         }
-        val validator = Validator()
-        val path = args[0]
-        if (!validator.validatePath(path)) throw Exception("Неверный путь")
-        val io = FileIO(path)
-        val service = CalculateService(io)
-        service.execute()
+        calculateFromFile(args[0])
     }
     catch(e: Exception){
         println(e.message)
     }
+
+}
+
+fun calculateFromConsole(){
+    val io = ConsoleIO()
+    val service = CalculateService(io)
+    service.execute()
+}
+
+fun calculateFromFile(path: String){
+    val validator = Validator()
+    if (!validator.validatePath(path)) throw Exception("Неверный путь")
+    val io = FileIO(path)
+    val service = CalculateService(io)
+    service.execute()
 }
